58e2f86c650d588714eae8273755d9c79410a7ef
Mose KE <eric24831684@gmail.com>
2022-05-05 13:41:22 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# 使用說明
+
+
+此 shell script 腳本用於將 `./content/*/<title>/title.md` 統一改成 `./content/*/<title>/index.md` 以利於 [Hugo Page Bundle (Leaf Bundle)](https://gohugo.io/content-management/page-bundles/) 使用。
+
+
+- 請將此腳本放在 Hugo root directory 執行。
+- 若 folder 內有其他 `.md` 檔，不會誤刪。
---
+++ rename_md_for_hugo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file_path=$(find ./content/ -mindepth 1 -type f -name '*.md' | cut -d '/' -f 5 --complement)
+
+for i in ${file_path};
+do
+
+file_name=$(echo -n ${i} | cut -d '/' -f 4)
+match_path=$(find ${i} -type f -name ${file_name}.md)
+dir_path=$(echo -n ${match_path} | cut -d '/' -f 5 --complement)
+
+#echo ${file_name}
+#echo ${match_path}
+#echo ${dir_path}
+
+# 如果同個目錄內有檔名與資料夾名稱不同的 .md 存在則 dir_path 會變空字串，就不執行 mv (rename)
+[[ "${dir_path}" != '' ]] && mv ${match_path} "${dir_path}/index.md"
+
+done
